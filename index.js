4c3556a413e624783ef65594700296d156114af8
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-04-25 22:16:25 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your code here
+
+
+function submitData (){
+   return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers:{
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify( {
+        name: "Steve",
+        email: "steve@steve.com"
+    }
+    ) }).then(res => res.json()).then(data => {document.body.innerHTML = data['id'];}).catch((error) => {alert("Bad things! Ragnarok!"); console.log(document.body.innerHTML = error.message);})
+}
